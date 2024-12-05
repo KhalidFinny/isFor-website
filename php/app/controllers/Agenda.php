ed751ed7400b5667773757b21aa2f68edba7731e
tora180405 <toradigda@gmail.com>
2024-12-05 22:31:28 +0700

    add agenda feature backend

--- php/app/controllers/Agenda.php
+++ php/app/controllers/Agenda.php
@@ -8,11 +8,48 @@ class Agenda extends Controller{
         if($role == 1){
             $this->saveLastVisitedPage();
             $data['agenda'] = $this->model('AgendaModel')->getAllAgenda();
+            
             $data['no'] =  1;
             $this->view('admin/manage-agenda', $data);
         }else{
             header('Location: ' . $this->getLastVisitedPage());
         }
     }
-    
+
+    public function addAgenda(){
+        if ($this->model('AgendaModel')->addAgenda($_POST) > 0) {
+            header('Location: ' . BASEURL . '/agenda');
+            echo "tambah data berhasil";
+        } else {
+            echo
+            '<script/>
+                    alert("tambah data gagal");
+                </script>';
+        }
+    }
+
+    public function getAgendaById(){
+        echo json_encode($this->model('AgendaModel')->getAgendaById($_POST['agenda_id']));
+    }
+
+    public function editAgenda(){
+        if ($this->model('AgendaModel')->editAgenda($_POST) > 0) {
+            header('Location: ' . BASEURL . '/agenda');
+            echo "ubah data berhasil";
+        } else {
+            echo
+            '<script/>
+                    alert("ubah data gagal");
+                </script>';
+        }
+    }
+
+    public function deleteAgenda($id){
+        if ($this->model('AgendaModel')->deleteAgenda($id) > 0) {
+            echo json_encode(['success' => true]);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Gagal menghapus agenda']);
+        }
+        exit();
+    }
 }
