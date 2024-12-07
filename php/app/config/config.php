61360f1b115283755d4a00816b78d81d0068faa4
dhiyaul10 <dhiyaulul108@gmail.com>
2024-12-07 09:37:28 +0700

    test

--- php/app/config/config.php
+++ php/app/config/config.php
@@ -6,7 +6,7 @@ define('PHOTOPROFILE', 'http://localhost/IsFor-website/php/app/img/profile/');
 define('ASSETS', 'http://localhost/IsFor-website/php/app/views/assets');
 define('LETTER', 'http://localhost/IsFor-website/php/app/letters');
 
-define('DB_HOST', '192.168.57.92');
+define('DB_HOST', '192.168.59.6');
 define('DB_USER', 'sa');
 define('DB_PASS', 'MySecureDB#456');
 define('DB_NAME', 'isfor_database');
