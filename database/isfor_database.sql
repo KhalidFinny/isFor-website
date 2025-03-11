c608c24a17a2bfc8ffb30c19d7e99cd2843972af
keyfarel <carinsmddd@gmail.com>
2025-03-11 22:08:39 +0700

    }

--- database/isfor_database.sql
+++ database/isfor_database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 07, 2025 at 02:27 PM
--- Server version: 8.0.30
--- PHP Version: 8.3.6
+-- Waktu pembuatan: 11 Mar 2025 pada 15.03
+-- Versi server: 8.0.30
+-- Versi PHP: 8.3.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `agenda`
+-- Struktur dari tabel `agenda`
 --
 
 CREATE TABLE `agenda` (
   `agenda_id` int NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
---
--- Error reading structure for table isfor_database.category: #1146 - Table &#039;isfor_database.category&#039; doesn&#039;t exist
--- Error reading data for table isfor_database.category: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near &#039;FROM `isfor_database`.`category`&#039; at line 1
-
--- --------------------------------------------------------
-
---
--- Table structure for table `galleries`
+-- Struktur dari tabel `galleries`
 --
 
 CREATE TABLE `galleries` (
   `gallery_id` int NOT NULL,
   `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `category` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `uploaded_by` int NOT NULL,
   `created_at` date DEFAULT NULL,
   `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
@@ -60,13 +52,13 @@ CREATE TABLE `galleries` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `letters`
+-- Struktur dari tabel `letters`
 --
 
 CREATE TABLE `letters` (
   `letter_id` int NOT NULL,
-  `title` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `file_url` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `file_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `status` int NOT NULL,
   `user_id` int NOT NULL,
   `date` date DEFAULT NULL,
@@ -76,25 +68,25 @@ CREATE TABLE `letters` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `research_outputs`
+-- Struktur dari tabel `research_outputs`
 --
 
 CREATE TABLE `research_outputs` (
   `research_output_id` int NOT NULL,
-  `file_url` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `file_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `uploaded_by` int NOT NULL,
   `uploaded_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `category` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `status` int NOT NULL,
-  `description` text COLLATE utf8mb4_general_ci NOT NULL,
-  `comment` varchar(500) COLLATE utf8mb4_general_ci DEFAULT NULL
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `comment` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `roadmaps`
+-- Struktur dari tabel `roadmaps`
 --
 
 CREATE TABLE `roadmaps` (
@@ -105,21 +97,72 @@ CREATE TABLE `roadmaps` (
   `topic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `roadmaps`
+--
+
+INSERT INTO `roadmaps` (`roadmap_id`, `year_start`, `year_end`, `category`, `topic`) VALUES
+(201, 2022, 2025, 'Smart ICT', 'LORA Systems'),
+(202, 2022, 2025, 'Smart ICT', 'LORA Mesh for IT Systems'),
+(203, 2022, 2025, 'Smart ICT', 'LORA for Smart Systems'),
+(204, 2022, 2025, 'IoT Applications', 'IoT for Urban Farming'),
+(205, 2022, 2025, 'IoT Applications', 'IoT for Freshwater Fish'),
+(206, 2022, 2025, 'IoT Applications', 'Smart Home'),
+(207, 2022, 2025, 'IoT Applications', 'IoT for Power Electric Distribution'),
+(208, 2022, 2025, 'Data Science & Analytics', 'Big Data Analysis'),
+(209, 2022, 2025, 'Data Science & Analytics', 'Natural Language Processing'),
+(210, 2022, 2025, 'Data Science & Analytics', 'Image Processing'),
+(211, 2022, 2025, 'Business Management', 'Governance Fiscal Independency'),
+(212, 2022, 2025, 'Business Management', 'Commercial Port Management System'),
+(213, 2022, 2025, 'Business Management', 'Document Archiving Management'),
+(214, 2026, 2028, 'Smart ICT', 'ICT for Industrial Automation'),
+(215, 2026, 2028, 'Smart ICT', 'Integrated Data Transaction'),
+(216, 2026, 2028, 'IoT Applications', 'Smart City'),
+(217, 2026, 2028, 'IoT Applications', 'Smart Ecosystem'),
+(218, 2026, 2028, 'IoT Applications', 'Smart Monitoring Systems'),
+(219, 2026, 2028, 'Data Science & Analytics', 'Voice Command Technology'),
+(220, 2026, 2028, 'Data Science & Analytics', 'Land & Building Mapping'),
+(221, 2026, 2028, 'Data Science & Analytics', 'Intelligence System'),
+(222, 2026, 2028, 'Data Science & Analytics', 'Integrated Information System: Trends & Prediction'),
+(223, 2026, 2028, 'Business Management', 'Customer Relation Management System'),
+(224, 2026, 2028, 'Business Management', 'Supply Chain Management'),
+(225, 2026, 2028, 'Business Management', 'Analytic of Documents Archiving'),
+(226, 2018, 2022, 'Smart ICT', 'Network Management System'),
+(227, 2018, 2022, 'Smart ICT', 'Network Topology'),
+(228, 2018, 2022, 'Smart ICT', 'Concept Electronics and IT Embedded System'),
+(229, 2018, 2022, 'IoT Applications', 'IoT System'),
+(230, 2018, 2022, 'IoT Applications', 'Sensors for IoT Systems'),
+(231, 2018, 2022, 'Data Science & Analytics', 'Decision Support System'),
+(232, 2018, 2022, 'Data Science & Analytics', 'Classification System'),
+(233, 2018, 2022, 'Data Science & Analytics', 'Prediction System'),
+(234, 2018, 2022, 'Data Science & Analytics', 'Cluster Analysis'),
+(235, 2018, 2022, 'Business Management', 'Digital Marketing'),
+(236, 2018, 2022, 'Business Management', 'Micro Commerce Management'),
+(237, 2018, 2022, 'Business Management', 'Digital Tax management');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `role`
+-- Struktur dari tabel `role`
 --
 
 CREATE TABLE `role` (
   `role_id` int NOT NULL,
-  `role_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+  `role_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `role`
+--
+
+INSERT INTO `role` (`role_id`, `role_name`) VALUES
+(1, 'admin'),
+(2, 'user');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `status`
+-- Struktur dari tabel `status`
 --
 
 CREATE TABLE `status` (
@@ -130,45 +173,52 @@ CREATE TABLE `status` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
   `user_id` int NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `profile_picture` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `profile_picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `role_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `username`, `email`, `profile_picture`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', 'admin@example.com', NULL, '$2y$10$EuZrLQWpmtPHoknQ8WoelOxNNeZJI1Amqu3JRQ2Uihz5R4bmUGhvi', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `agenda`
+-- Indeks untuk tabel `agenda`
 --
 ALTER TABLE `agenda`
   ADD PRIMARY KEY (`agenda_id`);
 
 --
--- Indexes for table `galleries`
+-- Indeks untuk tabel `galleries`
 --
 ALTER TABLE `galleries`
   ADD PRIMARY KEY (`gallery_id`),
   ADD KEY `fk_user` (`uploaded_by`);
 
 --
--- Indexes for table `letters`
+-- Indeks untuk tabel `letters`
 --
 ALTER TABLE `letters`
   ADD PRIMARY KEY (`letter_id`),
   ADD KEY `fk_user_id` (`user_id`);
 
 --
--- Indexes for table `research_outputs`
+-- Indeks untuk tabel `research_outputs`
 --
 ALTER TABLE `research_outputs`
   ADD PRIMARY KEY (`research_output_id`),
@@ -176,25 +226,25 @@ ALTER TABLE `research_outputs`
   ADD KEY `research_outputs_ibkf_2` (`status`);
 
 --
--- Indexes for table `roadmaps`
+-- Indeks untuk tabel `roadmaps`
 --
 ALTER TABLE `roadmaps`
   ADD PRIMARY KEY (`roadmap_id`);
 
 --
--- Indexes for table `role`
+-- Indeks untuk tabel `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`role_id`);
 
 --
--- Indexes for table `status`
+-- Indeks untuk tabel `status`
 --
 ALTER TABLE `status`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
@@ -203,82 +253,82 @@ ALTER TABLE `users`
   ADD KEY `role_id` (`role_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `agenda`
+-- AUTO_INCREMENT untuk tabel `agenda`
 --
 ALTER TABLE `agenda`
   MODIFY `agenda_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT for table `galleries`
+-- AUTO_INCREMENT untuk tabel `galleries`
 --
 ALTER TABLE `galleries`
   MODIFY `gallery_id` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `letters`
+-- AUTO_INCREMENT untuk tabel `letters`
 --
 ALTER TABLE `letters`
   MODIFY `letter_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
--- AUTO_INCREMENT for table `research_outputs`
+-- AUTO_INCREMENT untuk tabel `research_outputs`
 --
 ALTER TABLE `research_outputs`
   MODIFY `research_output_id` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `roadmaps`
+-- AUTO_INCREMENT untuk tabel `roadmaps`
 --
 ALTER TABLE `roadmaps`
-  MODIFY `roadmap_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201;
+  MODIFY `roadmap_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=238;
 
 --
--- AUTO_INCREMENT for table `role`
+-- AUTO_INCREMENT untuk tabel `role`
 --
 ALTER TABLE `role`
   MODIFY `role_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `status`
+-- AUTO_INCREMENT untuk tabel `status`
 --
 ALTER TABLE `status`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `galleries`
+-- Ketidakleluasaan untuk tabel `galleries`
 --
 ALTER TABLE `galleries`
   ADD CONSTRAINT `fk_user` FOREIGN KEY (`uploaded_by`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Constraints for table `letters`
+-- Ketidakleluasaan untuk tabel `letters`
 --
 ALTER TABLE `letters`
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Constraints for table `research_outputs`
+-- Ketidakleluasaan untuk tabel `research_outputs`
 --
 ALTER TABLE `research_outputs`
   ADD CONSTRAINT `research_outputs_ibfk_1` FOREIGN KEY (`uploaded_by`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `research_outputs_ibkf_2` FOREIGN KEY (`status`) REFERENCES `status` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
--- Constraints for table `users`
+-- Ketidakleluasaan untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE CASCADE;
