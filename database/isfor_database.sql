347855170aad980dbba5f066a57feca0ae7d9f46
keyfarel <carinsmddd@gmail.com>
2025-03-11 22:09:27 +0700

    feat(database): add missing seed data for roadmaps and role tables
    - Updated isfor_database.sql to include initial data for roadmaps and role.
    - Ensured the database script now properly seeds required tables for application functionality.

--- database/isfor_database.sql
+++ database/isfor_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 11 Mar 2025 pada 15.03
+-- Waktu pembuatan: 11 Mar 2025 pada 15.05
 -- Versi server: 8.0.30
 -- Versi PHP: 8.3.15
 
