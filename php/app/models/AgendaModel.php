c5c8204e1d39f9e9a457ca0740cfc0d4893bd519
keyfarel <carinsmddd@gmail.com>
2025-03-12 09:59:21 +0700

    feat(agenda-model): migrasi model agenda dari SP SQL Server ke query MySQL biasa
    - Mengganti pemanggilan stored procedure dengan query MySQL standar.
    - Menyesuaikan struktur dan logika model agar sesuai dengan sintaks MySQL.

--- php/app/models/AgendaModel.php
+++ php/app/models/AgendaModel.php
@@ -1,34 +1,47 @@
-<?php 
+<?php
 
-class AgendaModel{
+class AgendaModel
+{
     private $table = 'agenda';
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database;
     }
 
-    public function getAllAgenda() {
-        $this->db->query('EXEC sp_GetAllAgenda');
+    public function getAllAgenda()
+    {
+        $query = 'SELECT *, ROW_NUMBER() OVER (ORDER BY agenda_id) AS number FROM agenda';
+        $this->db->query($query);
         return $this->db->resultSet();
     }
 
-    public function getAgendaById($id) {
-        $this->db->query('EXEC sp_GetAgendaById :id');
+    public function getAgendaById($id)
+    {
+        $query = 'SELECT * FROM agenda WHERE agenda_id = :id';
+        $this->db->query($query);
         $this->db->bind(':id', $id);
         return $this->db->single();
     }
 
-    public function addAgenda($data) {
-        $this->db->query('EXEC sp_AddAgenda :title, :description');
+    public function addAgenda($data)
+    {
+        $query = 'INSERT INTO agenda (title, description) VALUES (:title, :description)';
+        $this->db->query($query);
         $this->db->bind(':title', $data['title']);
         $this->db->bind(':description', $data['description']);
         $this->db->execute();
         return $this->db->rowCount();
     }
 
-    public function editAgenda($data) {
-        $this->db->query('EXEC sp_EditAgenda :id, :title, :description');
+    public function editAgenda($data)
+    {
+        $query = 'UPDATE agenda 
+                  SET title = :title, 
+                      description = :description 
+                  WHERE agenda_id = :id';
+        $this->db->query($query);
         $this->db->bind(':id', $data['agenda_id']);
         $this->db->bind(':title', $data['title']);
         $this->db->bind(':description', $data['description']);
@@ -36,8 +49,10 @@ class AgendaModel{
         return $this->db->rowCount();
     }
 
-    public function deleteAgenda($id) {
-        $this->db->query('EXEC sp_DeleteAgenda :id');
+    public function deleteAgenda($id)
+    {
+        $query = 'DELETE FROM agenda WHERE agenda_id = :id';
+        $this->db->query($query);
         $this->db->bind(':id', $id);
         $this->db->execute();
         return $this->db->rowCount();
